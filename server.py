7d2bc4c4bb592f59f42b5903514bde6dc84377f0
evanxliu1 <148828269+evanxliu1@users.noreply.github.com>
2023-10-25 16:37:51 -0700

    Add files via upload

--- server.py
+++ server.py
@@ -1,20 +1,9 @@
-from wsgiref.simple_server import make_server
-from pyramid.config import Configurator
-from pyramid.response import Response
-import os
+from flask import Flask
+app = Flask(__name__)
 
-def hello_world(request):
-    name = os.environ.get('NAME')
-    if name == None or len(name) == 0:
-        name = "world"
-    message = "Hello, " + name + "!\n"
-    return Response(message)
+@app.route('/')
+def hello_world():
+   return "Hello, World!"
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT"))
-    with Configurator() as config:
-        config.add_route('hello', '/')
-        config.add_view(hello_world, route_name='hello')
-        app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', port, app)
-    server.serve_forever()
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=5000)
