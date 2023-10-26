4140789fb63935e13393b1b432fca5219dece3f0
evanxliu1 <148828269+evanxliu1@users.noreply.github.com>
2023-10-25 22:21:12 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import spotipy.oauth2
 from spotipy.oauth2 import SpotifyOAuth
 import spotipy
-from flask import Flask
+from flask import Flask, request
 
 
 CLIENT_ID = "679be63979e04cc496a45c75d2afa493"
