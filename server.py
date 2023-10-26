30b9e916ea24ddf7e281e18c22b58cb6ad0b71f0
evanxliu1 <148828269+evanxliu1@users.noreply.github.com>
2023-10-26 00:29:14 -0700

    Add files via upload

--- server.py
+++ server.py
@@ -1,22 +1,26 @@
 import spotipy.oauth2
 from spotipy.oauth2 import SpotifyOAuth
 import spotipy
-from flask import Flask, request
+from flask import Flask, request, session, render_template
 
 
 CLIENT_ID = "679be63979e04cc496a45c75d2afa493"
 CLIENT_SECRET = "6ecdc388b44a4c4fb9adf33afb833fb9"
 port = 5000
-redirectURI = "https://wfbjw6avrv.us-east-1.awsapprunner.com/callback"
+#port = 8000
+redirectURI = "https://wfbjw6avrv.us-east-1.awsapprunner.com:5000/callback"
+#redirectURI = "http://127.0.0.1:8000/callback"
 sp_oauth = SpotifyOAuth(redirect_uri=redirectURI, client_id=CLIENT_ID,
-                         client_secret=CLIENT_SECRET, scope='user-read-recently-played')
+                         client_secret=CLIENT_SECRET, scope='user-read-recently-played user-top-read')
 
 auth_url = sp_oauth.get_authorize_url()
-#auth_url = 'youtube.com'
+sp = spotipy.Spotify()
 app = Flask(__name__)
+app.secret_key = 'BAD_SECRET_KEY'
 @app.route("/")
 def hello_world():
-    return f'<a href="{auth_url}">login to spotify test 3!</a>'
+    return f'<a href="{auth_url}">login to spotify test 2!</a>'
+
 
 @app.route('/callback')
 def callback():
@@ -24,20 +28,33 @@ def callback():
      authorization_code = request.args.get('code')
      print(authorization_code)
      if authorization_code:
-         #print("Authorization Code:", authorization_code)
          access_token = sp_oauth.get_access_token(authorization_code,check_cache=False)
-         #print("Access Token:", access_token)
-         sp = spotipy.Spotify()
-
          sp.set_auth(access_token['access_token'])
-         results = sp.current_user_recently_played(limit=20)
-         #print(results)
-         for item in results['items']:
-             track_name = item['track']['name']
-             #print("Name:", track_name)
-             tracklist.append(track_name)
-     return tracklist
+         session['session_access_token'] = access_token['access_token']
+     return render_template("top_track.html")
+
+def getTopTrack(term):
+    print("Sessiontest", session['session_access_token'])
+    sp.set_auth(session['session_access_token'])
+    topTracks = sp.current_user_top_tracks(limit=10, time_range=term)
+    return topTracks
+
+
+@app.route('/toptracks', methods=['GET', 'POST'])
+def topTracks():
+    topTrack = []
+    tracklist = []
+    if request.method == 'POST':
+        topTrack = getTopTrack(request.form.get('action'))  # do something
+        for item in topTrack['items']:
+            print(item)
+            track_name = item['name']
+            tracklist.append(track_name)
+        return render_template('result.html', tracknamelist=tracklist)
+    elif request.method == 'GET':
+        return render_template('top_track.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
+
 
